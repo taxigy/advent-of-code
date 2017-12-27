24ae37a4b035a7b10eb6b71b15d34127d75e9074
Rishat Muhametshin <rishat@rishat.us>
2017-12-27 22:34:54 +0100

    day 24

--- 2017/src/day24.clj
+++ 2017/src/day24.clj
@@ -19,10 +19,31 @@
   ([r t m] (let [{:keys [head tail]} m]
              (if (empty? head)
                r
-               (map #(possible-steps (cons (apply + t %) r)
-                                     (apply + t %)
-                                     (split (second %) tail))
+               (map (fn [h] (possible-steps (cons (apply + t h) r)
+                                            (apply + t h)
+                                            (split (second h) (concat tail
+                                                                      (filter #(not= h %) head)))))
                     head)))))
 
+(defn flat [coll]
+  (mapcat #(if (every? coll? %)
+             (flat %)
+             (list %))
+          coll))
+
+(defn take-longest [coll]
+  (let [c (sort #(compare (count %2) (count %1)) coll)
+        longest (take-while #(= (count %) (count (first c))) c)]
+    longest))
+
+(defn take-strongest [coll]
+  (let [m (apply max (map first coll))
+        strongest (filter #(= m (first %)) coll)]
+    strongest))
+
 (defn calculate [input]
-  (apply max (flatten (possible-steps input))))
+  (let [steps (possible-steps input)
+        fsteps (flat nested-result)
+        longest (take-longest fsteps)
+        strongest (take-strongest longest)]
+    (first (first strongest))))
